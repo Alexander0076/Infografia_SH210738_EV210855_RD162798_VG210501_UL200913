e7615ea88dac5a3ea330dbbdd0cb7b623504c7a5
Alexander0076 <uliseselias007@gmail.com>
2021-09-03 00:19:28 -0600

    Imagen

---
+++ js/imagenes.js
@@ -0,0 +1,17 @@
+const imgWidth = 46;
+const imgHeight = 50;
+const imagenes = {
+    "uno":{
+        velocidad: 5,
+        animaciones: [
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+            {x: 0, y: 0},
+        ]
+    }
+} 
